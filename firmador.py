f61721bc4bfc27b67b157b63f1e34c5cf53e086b
alvaro <aleiva97@gmail.com>
2022-08-30 14:47:34 -0400

    firma indivudial func

--- firmador.py
+++ firmador.py
@@ -2,20 +2,101 @@
 import datetime
 from cryptography.hazmat import backends
 from cryptography.hazmat.primitives.serialization import pkcs12
-import fitz
+# import fitz
 from endesive.pdf import cms
 import json
 import base64
 from urllib.request import urlopen
 
 import logging
-def firmar(contraseña, certificado, pdf,bufferCert):
+
+def certificarPdf(contraseña, linkpdf):
     
-    
-    url = "https://websalsign.s3.amazonaws.com/liquidacionSigned.pdf"
+    link = linkpdf
+    url = f"https://websalsign.s3.amazonaws.com/{link}"
+
+    print(url)
+
+    urlp12 = 'https://websalsign.s3.amazonaws.com/2676/websal.p12'
 
     with urlopen(url) as file:
       content = file.read()
+        
+    # print(content)
+
+    with urlopen(urlp12) as file:
+      contentp12 = file.read()
+    # print(content)
+
+
+    print('-------------------------------------------')
+
+
+    date = datetime.datetime.utcnow() 
+    date = date.strftime("D:%Y%m%d%H%M%S+00'00'")
+    dct = {
+        "aligned": 0,
+        "sigflags": 3,
+        "sigflagsft": 132,
+        "sigpage": 0,
+        "sigbutton": True,
+        "sigfield": "Signature1",
+        "auto_sigfield": True,
+        "sigandcertify": True,
+        # "signaturebox": (470, 840, 570, 640),
+        #  "signaturebox": (0, 0, 590, 155),
+        # "signature": "Aquí va la firma 22323",
+        # "signature_img": "signature_test.png",
+        "contact": "contacto@websal.com",
+        "location": "Ubicación",
+        "signingdate": date,
+        "reason": "Razón",
+        "password": contraseña,
+    }
+    # with open("cert.p12", "rb") as fp:
+    p12 = pkcs12.load_key_and_certificates(
+        contentp12, contraseña.encode("ascii"), backends.default_backend()
+    )
+
+    #datau = open(fname, "rb").read()
+
+    
+    datau = content
+    
+    # print(datau)
+
+    # print(type(certificado.read()))
+
+    datas = cms.sign(datau, dct, p12[0], p12[1], p12[2], "sha256")
+
+    # print(type(datas))
+
+    base64_encoded_data = base64.b64encode(datas)
+    base64_message = base64_encoded_data.decode('utf-8')
+    
+ 
+ 
+
+    return datau, datas
+    """
+    fname = "test.pdf"
+    with open(fname, "wb") as fp:
+        fp.write(datau)
+        fp.write(datas)
+    """
+
+def firmar(contraseña, certificado, pdf,bufferCert, linkpdf):
+    
+    link = linkpdf
+    url = f"https://websalsign.s3.amazonaws.com/{link}"
+
+    print(url)
+
+    with urlopen(url) as file:
+      content = file.read()
+    # print(content)
+
+   
     # print(content)
 
 
@@ -23,9 +104,11 @@ def firmar(contraseña, certificado, pdf,bufferCert):
 
     # leemos el buffer del certificado y los transformamos a bytes
     x = bufferCert['data']
+   
+
 
     byte_arrayCert = bytearray(x)
-
+    print(byte_arrayCert)
 
     date = datetime.datetime.utcnow() 
     date = date.strftime("D:%Y%m%d%H%M%S+00'00'")
--- server.py
+++ server.py
@@ -1,6 +1,7 @@
 import io
 from flask import Flask, render_template, request, send_file
 from firmador import firmar
+from firmador import certificarPdf
 import base64
 import json
 
@@ -27,11 +28,43 @@ def procesar():
     # print(json)
 
     bufferCert = json['bufferCert']
+    # print(json)
+    linkpdf = json['linkpdf']
 
     contraseña = ''
     archivo_pdf_para_enviar_al_cliente = io.BytesIO()
     try:
-        datau, datas = firmar(contraseña, firma, pdf,bufferCert)
+        datau, datas = firmar(contraseña, firma, pdf,bufferCert,linkpdf)
+        archivo_pdf_para_enviar_al_cliente.write(datau)
+        archivo_pdf_para_enviar_al_cliente.write(datas)
+        archivo_pdf_para_enviar_al_cliente.seek(0)
+        base64send= base64.b64encode(archivo_pdf_para_enviar_al_cliente.read()).decode()
+        # return send_file(archivo_pdf_para_enviar_al_cliente, mimetype="application/pdf",
+        #                  download_name="firmado" + ".pdf",
+        #                  as_attachment=True)
+        return base64send
+    except ValueError as e:
+        return "Error firmando: " + str(e) + " . Se recomienda revisar la contraseña y el certificado"
+
+
+
+
+@app.route('/certificar',  methods=['POST'])
+def certificar():
+
+    content_type = request.headers.get('Content-Type')
+    if (content_type == 'application/json'):
+        json = request.json
+    # print(json)
+
+    linkpdf = json['linkpdf']
+    
+    # linkpdf = 'liquidacion.pdf'
+
+    contraseña = ''
+    archivo_pdf_para_enviar_al_cliente = io.BytesIO()
+    try:
+        datau, datas = certificarPdf(contraseña, linkpdf)
         archivo_pdf_para_enviar_al_cliente.write(datau)
         archivo_pdf_para_enviar_al_cliente.write(datas)
         archivo_pdf_para_enviar_al_cliente.seek(0)
